8ff0035f206452cf89002085e1d8cc483aca0a8e
pmanjuna <pmanjuna@usc.edu>
2016-09-30 06:34:28 -0700

    traceroute working. need optimization.

--- router.c
+++ router.c
@@ -7,12 +7,12 @@
 #include <sys/socket.h>
 //#include <netinet/in.h>
 #include <arpa/inet.h>
-
+#include <netinet/ip_icmp.h>
 #include <netinet/ether.h>
 #include <unistd.h>
 #include "arp.h"
 char dev[5];
-
+pcap_t* descr;
 int icount = 3;
 int n = 3;
 typedef struct output_if_pcap {
@@ -36,6 +36,7 @@ typedef struct ip_mac {
 EthMacPair ethMacPair[3];
 arp_t table[5];
 Output output[2];
+uint32_t my_ip[3] = { 16908554, 33622538, 16976394 };
 /* IP header */
 typedef struct sniff_ip {
 	u_char ip_vhl; /* version << 4 | header length >> 2 */
@@ -54,7 +55,7 @@ typedef struct sniff_ip {
 	struct in_addr ip_src, ip_dst; /* source and dest address */
 } Iphdr;
 
-u_int16_t handle_ethernet(u_char *args, const struct pcap_pkthdr* pkthdr,
+void handle_ethernet(u_char *args, const struct pcap_pkthdr* pkthdr,
 		const u_char* packet);
 
 /* looking at ethernet headers */
@@ -67,6 +68,28 @@ void my_callback(u_char *args, const struct pcap_pkthdr* pkthdr,
 	 } else if (type == ETHERTYPE_ARP) { handle arp packet
 	 } else if (type == ETHERTYPE_REVARP) { handle reverse arp packet
 	 }*/
+}
+
+unsigned short in_cksum(unsigned short *addr, int len) {
+	int nleft = len;
+	int sum = 0;
+	unsigned short *w = addr;
+	unsigned short answer = 0;
+
+	while (nleft > 1) {
+		sum += *w++;
+		nleft -= 2;
+	}
+
+	if (nleft == 1) {
+		*(unsigned char *) (&answer) = *(unsigned char *) w;
+		sum += answer;
+	}
+
+	sum = (sum >> 16) + (sum & 0xffff);
+	sum += (sum >> 16);
+	answer = ~sum;
+	return (answer);
 }
 
 unsigned short checksum(Iphdr *ip, int len) {
@@ -87,18 +110,17 @@ unsigned short checksum(Iphdr *ip, int len) {
 	return (~sum);
 }
 
-u_int16_t handle_ethernet(u_char *args, const struct pcap_pkthdr* pkthdr,
+void handle_ethernet(u_char *args, const struct pcap_pkthdr* pkthdr,
 		const u_char* packet) {
-	int i, j;
-	struct ether_header *eptr; /* net/ethernet.h */
 
-	/* lets start with the ether header... */
+	struct ether_header *eptr;
 	eptr = (struct ether_header *) packet;
 	Iphdr *ip = (Iphdr*) (packet + SIZE_ETHERNET);
-
-	/* check to see if we have an ip packet */
+	in_addr_t temp = ip->ip_src.s_addr;
+	ip->ip_ttl = ip->ip_ttl - 1;
 	if (ntohs(eptr->ether_type) == ETHERTYPE_IP) {
-		printf("dst ip = %d\n", ip->ip_dst.s_addr);
+		printf("dst ip = %d , ttl = %d , protocol = %d\n", ip->ip_dst.s_addr,
+				ip->ip_ttl, ip->ip_p);
 		fprintf(stdout, "Dev: %s INCOMING: SRC MAC: %s", dev,
 				ether_ntoa((const struct ether_addr *) &eptr->ether_shost));
 		fprintf(stdout, " DEST MAC: %s ",
@@ -107,11 +129,158 @@ u_int16_t handle_ethernet(u_char *args, const struct pcap_pkthdr* pkthdr,
 
 		//send the modified packet
 
+		//check if dst is my ip
+		if (ip->ip_ttl == 0) {
+			struct icmp *icmp_hdr;
+			int ip_len, icmp_len;
+			icmp_hdr =
+					(struct icmp *) ((unsigned char *) ip + sizeof(struct ip));
+			char buf[28];
+			memcpy(buf, ip, 28);
+			int len = pkthdr->len - sizeof(struct ether_header)
+					- sizeof(struct ip);
+			memset(icmp_hdr, 0, len);
+			memcpy(icmp_hdr->icmp_data, buf, 28);
+
+			if (((ip->ip_dst.s_addr != 33335488)
+					&& ((ip->ip_dst.s_addr == my_ip[0])
+							|| (ip->ip_dst.s_addr == my_ip[1])
+							|| (ip->ip_dst.s_addr == my_ip[2])))) {
+				//dest unreachable
+				icmp_hdr->icmp_type = 3;
+				icmp_hdr->icmp_code = 3;
+				printf("Dest host unreachable.\n");
+
+			} else {
+
+				//timeout exceeded
+				icmp_hdr->icmp_type = 11;
+				icmp_hdr->icmp_code = 0;
+				printf("timeout exceeded.\n");
+
+			}
+
+			icmp_hdr->icmp_cksum = 0;
+			icmp_hdr->icmp_id = 0;
+			icmp_hdr->icmp_seq = 0;
+			ip_len = ntohs(ip->ip_len);
+			icmp_len = ip_len - sizeof(struct ip);
+			icmp_hdr->icmp_cksum = in_cksum((unsigned short *) icmp_hdr,
+					icmp_len);
+
+			ip->ip_tos = 0;
+			ip->ip_off = 0;
+			ip->ip_ttl = 255;
+			ip->ip_p = IPPROTO_ICMP;
+			ip->ip_sum = 0;
+			ip->ip_dst.s_addr = ip->ip_src.s_addr;
+			ip->ip_src.s_addr = 16908554;
+			ip->ip_sum = checksum(ip, IP_HL(ip) * 4);
+
+			// modify mac
+			for (int j = 0; j < 5; j++) {
+				/*printf("inside for dest ip = %d\n", ip->ip_dst.s_addr);*/
+				if (table[j].destination_ip == ip->ip_dst.s_addr) {
+					printf("inside if\n");
+
+					memcpy(eptr->ether_shost, table[j].source_mac,
+							sizeof(table[j].source_mac));
+					memcpy(eptr->ether_dhost, table[j].destination_mac,
+							sizeof(table[j].destination_mac));
+					struct ether_header *sptr = (struct ether_header *) packet;
+
+					fprintf(stdout, "icmp pkt outgoing: source: %s ",
+							ether_ntoa(
+									(const struct ether_addr *) &sptr->ether_shost));
+					fprintf(stdout, "icmp pkt destination: %s \n",
+							ether_ntoa(
+									(const struct ether_addr *) &sptr->ether_dhost));
+					break;
+
+				}				//end of if
+			}				// end of for
+
+			if (pcap_inject(descr, packet, pkthdr->len) == -1) {
+				pcap_perror(descr, 0);
+				pcap_close(descr);
+				exit(1);
+			}				// end of inject if
+			printf("Inject completed on %s\n", dev);
+
+		}
+	}
+
+	if (((ip->ip_dst.s_addr != 33335488)
+			&& ((ip->ip_dst.s_addr == my_ip[0])
+					|| (ip->ip_dst.s_addr == my_ip[1])
+					|| (ip->ip_dst.s_addr == my_ip[2])))) {
+
+		if ((ip->ip_ttl != 0) && ip->ip_p == IPPROTO_ICMP) {
+
+			struct icmp *icmp_hdr;
+			int ip_len, icmp_len;
+
+			icmp_hdr =
+					(struct icmp *) ((unsigned char *) ip + sizeof(struct ip));
+
+			if (icmp_hdr->icmp_type == ICMP_ECHO) {
+				/* Prepare outgoing ICMP header. */
+				icmp_hdr->icmp_type = 0; //11;
+				icmp_hdr->icmp_code = 0;
+				icmp_hdr->icmp_cksum = 0;
+				ip_len = ntohs(ip->ip_len);
+				icmp_len = ip_len - sizeof(struct ip);
+				printf("ICMP_ECHO request.\n");
+				icmp_hdr->icmp_cksum = in_cksum((unsigned short *) icmp_hdr,
+						icmp_len);
+			} //if echo check end
+
+			//modify IP header
+
+			ip->ip_sum = 0;
+			ip->ip_src.s_addr = ip->ip_dst.s_addr;
+			ip->ip_dst.s_addr = temp;
+			ip->ip_ttl = 255;
+			ip->ip_sum = checksum(ip, IP_HL(ip) * 4);
+
+			// modify mac
+			for (int j = 0; j < 5; j++) {
+				printf("inside for dest ip = %d\n", ip->ip_dst.s_addr);
+				if (table[j].destination_ip == ip->ip_dst.s_addr) {
+					printf("inside if\n");
+					memcpy(eptr->ether_shost, table[j].source_mac,
+							sizeof(table[j].source_mac));
+					memcpy(eptr->ether_dhost, table[j].destination_mac,
+							sizeof(table[j].destination_mac));
+					struct ether_header *sptr = (struct ether_header *) packet;
+
+					fprintf(stdout, "icmp pkt outgoing: source: %s ",
+							ether_ntoa(
+									(const struct ether_addr *) &sptr->ether_shost));
+					fprintf(stdout, "icmp pkt destination: %s \n",
+							ether_ntoa(
+									(const struct ether_addr *) &sptr->ether_dhost));
+					break;
+
+				}				//end of if
+			}				// end of for
+
+			if (pcap_inject(descr, packet, pkthdr->len) == -1) {
+				pcap_perror(descr, 0);
+				pcap_close(descr);
+				exit(1);
+			}				// end of inject if
+			printf("Inject completed on %s\n", dev);
+
+		} //end of my_ip if
+
+	} else {
+		int i, j;
 		for (i = 0; i < 5; i++) {
 			if (table[i].destination_ip == ip->ip_dst.s_addr) {
 
 				//Decrement TTL
-				ip->ip_ttl = ip->ip_ttl - 1;
+				//ip->ip_ttl = ip->ip_ttl - 1;
 				ip->ip_sum = 0;
 				ip->ip_sum = checksum(ip, IP_HL(ip) * 4);
 
@@ -135,9 +304,9 @@ u_int16_t handle_ethernet(u_char *args, const struct pcap_pkthdr* pkthdr,
 
 		for (j = 0; j < 2; j++) {
 			printf(" table iface = %d, output iface =%d \n", table[i].iface,
-					output[j].iface);
-			printf(" table iface = %s, output iface =%s \n",
-					table[i].iface_name, output[j].iface_name);
+			 output[j].iface);
+			 printf(" table iface = %s, output iface =%s \n",
+			 table[i].iface_name, output[j].iface_name);
 			if (output[j].iface == table[i].iface) {
 				if (pcap_inject(output[j].pcap, packet, pkthdr->len) == -1) {
 					pcap_perror(output[j].pcap, 0);
@@ -148,16 +317,15 @@ u_int16_t handle_ethernet(u_char *args, const struct pcap_pkthdr* pkthdr,
 				break;
 			}
 		}
-
 	}
+	// end of my_ip for
 
-	return eptr->ether_type;
-}
+}				// end of if ETHERTYPE_IP
 
 int main(int argc, char **argv) {
 	int p = 0;
 	char errbuf[PCAP_ERRBUF_SIZE];
-	pcap_t* descr;
+
 	struct bpf_program fp; /* hold compiled program     */
 	bpf_u_int32 netp = 0; /* ip                        */
 	u_char* args = NULL;
